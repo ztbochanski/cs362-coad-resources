c5c02149aabb84c03e025211101ba822f37bd47b
lukereynoldspi <lukereynoldspi@gmail.com>
2022-01-25 10:40:15 -0800

    finished region_spec

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+  it "exists" do
+    Region.new
+  end
+
+  describe "attributes" do
+    it "has a name" do
+      region = Region.new
+      expect(region).to respond_to(:name)
+    end
+  end
+
+  describe "#to_s" do
+    it "returns the same" do
+      name = 'Mt. Hood'
+      region = Region.new(name: name)
+      expect(region.to_s).to eq(name)
+    end
+  end
+
 end
