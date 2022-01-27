8d4a8a23d5e2236d0495244161fb901ee12209fc
zach <ztbochanski@gmail.com>
2022-01-27 11:51:56 -0800

    add name and email validation test to organization spec

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,8 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
-  it "exists" do
-    Organization.new
+  describe "validations" do
+
+    it "is not valid without and email" do
+      organization = Organization.new(email: "fake@fake.com")
+      expect(organization).to be_invalid
+    end
+
+    it "is not valid without a name" do
+      organization = Organization.new(name: "fakename")
+      expect(organization).to be_invalid
+    end
+
   end
 
 end
