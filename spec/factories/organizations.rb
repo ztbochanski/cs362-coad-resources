24298a5d0b48622852107fe870ed4cb78d8662bf
zach <ztbochanski@gmail.com>
2022-02-02 20:35:41 -0800

    add params to organizations factory

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,3 +1,14 @@
 FactoryBot.define do
-    
+    factory :organization do 
+      email { "fake@fake.com" }
+      name { "fakename" }
+      phone { "fakephone" }
+      status { :approved }
+      primary_name { "fake primary name" }
+      secondary_name { "fake secondary name" }
+      secondary_phone { "fake secondary phone" }
+      title { "fake title" }
+      description { "fake description" }
+      transportation { :yes }
+    end
 end
