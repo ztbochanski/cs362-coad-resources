b305f4ee7d8ad11b73f62c28c06e45cd13422282
zach <ztbochanski@gmail.com>
2022-02-02 20:54:43 -0800

    refactor validation to be falsey using factory object

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,58 +2,47 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
+  let (:organization){
+    build_stubbed(:organization)
+  }
+
   describe "validations" do
 
-    it "is not valid without and email" do
-      organization = Organization.new(email: "fake@fake.com")
+    it "is not valid without an email" do
+      organization = build(:organization, email: nil)
       expect(organization).to be_invalid
     end
 
     it "is not valid without a name" do
-      organization = Organization.new(name: "fakename")
+      organization = build(:organization, name: nil)
       expect(organization).to be_invalid
     end
 
     it "is not valid without a phone" do
-      organization = Organization.new(phone: "fakephone")
+      organization = build(:organization, phone: nil)
       expect(organization).to be_invalid
     end
 
     it "is not valid without a status" do
-      organization = Organization.new(status: :approved)
+      organization = build(:organization, status: nil)
       expect(organization).to be_invalid
     end
 
     it "is not valid without a primary name" do
-      organization = Organization.new(primary_name: "fake primary name")
+      organization = build(:organization, primary_name: nil)
       expect(organization).to be_invalid
     end
 
     it "is not valid without a secondary name" do
-      organization = Organization.new(secondary_name: "fake secondary name")
+      organization = build(:organization, secondary_name: nil)
       expect(organization).to be_invalid
     end
 
     it "is not valid without a secondary phone" do
-      organization = Organization.new(secondary_phone: "fake secondary phone")
+      organization = build(:organization, secondary_phone: nil)
       expect(organization).to be_invalid
     end
-
-    it "is not valid without a title" do
-      organization = Organization.new(title: "fake title")
-      expect(organization).to be_invalid
-    end
-
-    it "is not valid without a description" do
-      organization = Organization.new(description: "fake description")
-      expect(organization).to be_invalid
-    end
-
-    it "is not valid without transportation" do
-      organization = Organization.new(transportation: :yes)
-      expect(organization).to be_invalid
-    end
-
+    
   end
 
 end
