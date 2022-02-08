dcd701a998f5cf14a2bbc8e7bfcd9ac3f6f45f75
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-02-08 11:43:41 -0800

    started the test for tickets haven't actuualy made a facotry for the scope attriubutes. This is just the code from class to help start the ticket test

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+    describe "scopes" do
+        describe "open" do
+            open_ticket = Ticket.create!(closed: false, organization: nil)
+            closed_ticket = Ticket.create!(closed: true)
+
+            results = Ticket.open
+            expect(results).to include(open_ticket)
+            expect(results).to_not include(closed_ticket)
+        end
+    end
+
 end
+
+
+open_tickets = Ticket.open()
