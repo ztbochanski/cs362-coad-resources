e31df82985857a7e6b6def20b66bda59d1085ae7
zach <ztbochanski@gmail.com>
2022-02-09 15:43:00 -0800

    remove unspecified check

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let (:user) { build_stubbed(:user)
-  }
+  let (:user) { build_stubbed(:user)}
   
   describe "attributes" do 
     it "exist" do
@@ -11,17 +10,10 @@ RSpec.describe User, type: :model do
   end
   
   describe "#to_s" do
-    it "returns the name" do
-      name = 'Joe Mama'
-      user = User.new(name: name)
-      expect(user.to_s).to eq(name)
-    end
-  end
-  
-  describe "::unspecified" do
-    it "returns a user with the name 'Unspecified" do
-      user = User.unspecified
-      expect(user.name).to eq('Unspecified')
+    it "returns the email" do
+      email = 'fake@email.com'
+      user = User.new(email: email)
+      expect(user.to_s).to eq(email)
     end
   end
   
@@ -36,5 +28,6 @@ RSpec.describe User, type: :model do
       user = User.new(email: nil)
       expect(user).to be_invalid
     end
+  end
 
 end
