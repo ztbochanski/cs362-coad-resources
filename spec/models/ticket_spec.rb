6c545e917aca4a63ca568239942070ceeb0fa3c6
zach <ztbochanski@gmail.com>
2022-02-09 18:01:15 -0800

    add test for open scope
    let variabels created to test and open ticket and a closed ticket

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,18 +2,35 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
-    describe "scopes" do
-        describe "open" do
-            open_ticket = Ticket.create!(closed: false, organization: nil)
-            closed_ticket = Ticket.create!(closed: true)
+    let (:region) { create(:region) }
+    let (:resource_category) { create(:resource_category) }
 
+    let (:open_ticket) { Ticket.create!(
+        closed: false,
+        phone: "1-555-666-2244",
+        name: "fake ticket",
+        organization: nil,
+        region: region,
+        resource_category: resource_category,
+    )}
+
+    let (:closed_ticket) { Ticket.create!(
+        closed: true,
+        phone: "1-555-666-2244",
+        name: "fake ticket",
+        organization: nil,
+        region: region,
+        resource_category: resource_category,
+    )}
+
+    describe "scopes" do
+        
+        it "returns open tickets from database" do
             results = Ticket.open
             expect(results).to include(open_ticket)
             expect(results).to_not include(closed_ticket)
         end
+
     end
 
 end
-
-
-open_tickets = Ticket.open()
