2cab0b0e68e86c6e0230710d1fbd975c0b63e70e
zach <ztbochanski@gmail.com>
2022-02-12 15:59:44 -0800

    fix tickets factory to proper syntax
    make sure to run tests before commiting

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :ticket do 
-    closed: false,
-    phone: "1-555-666-2244",
-    name: "fake ticket",
-    organization: nil,
-    region,
-    resource_category,
+    closed { false }
+    phone { "1-555-666-2244" }
+    name { "fake ticket" }
+    organization { nil }
+    region
+    resource_category
   end
 end
