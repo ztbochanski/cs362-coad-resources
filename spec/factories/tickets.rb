a023c74102027ef129b6a05a97e8e2200c9af171
zach <ztbochanski@gmail.com>
2022-02-12 15:47:12 -0800

    add ticket factory
    note that region and resource category can either be built as fake objects or created in the database by factory bot

---
+++ spec/factories/tickets.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :ticket do 
+    closed: false,
+    phone: "1-555-666-2244",
+    name: "fake ticket",
+    organization: nil,
+    region,
+    resource_category,
+  end
+end
