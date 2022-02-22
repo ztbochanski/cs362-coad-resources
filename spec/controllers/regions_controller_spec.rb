0e85216029078d3511938cd6370bd884b38a81c5
zach <ztbochanski@gmail.com>
2022-02-22 11:40:39 -0800

    add a persion accessing a region not logged in test block add get index and redirect to a sign in screen test

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
+  
+  describe "a person accessing a region" do
+
+    it "redirets to the sign in screen" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
 
 end
