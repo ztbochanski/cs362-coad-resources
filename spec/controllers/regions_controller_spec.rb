e80ccc791182f7ee37fca55e5e59ec1f12bde1d1
zach <ztbochanski@gmail.com>
2022-02-27 18:40:27 -0800

    fix grammer

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
   
-  describe "a unauthenticated person accessing a region" do
+  describe "an unauthenticated person accessing a region" do
     it "redirets to the sign in screen" do
       get :index
       expect(response).to redirect_to(new_user_session_path)
