6424e25ec8f8e5ca52dfd52316d26943fcbd6202
zach <ztbochanski@gmail.com>
2022-02-27 17:36:01 -0800

    add index and show redirects to non logged in user

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 RSpec.describe RegionsController, type: :controller do
   
   describe "a unauthenticated person accessing a region" do
-
     it "redirets to the sign in screen" do
       get :index
       expect(response).to redirect_to(new_user_session_path)
-      get :show, params: {id: "FAKE"}
-      get :new
+      get :show, params: {id: "Fake"}
       expect(response).to redirect_to(new_user_session_path)
+      
     end
   end
 
