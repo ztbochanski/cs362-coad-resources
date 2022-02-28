9d7288e60ccf04bffd08c0e06f52778b7d2aef92
zach <ztbochanski@gmail.com>
2022-02-27 16:32:30 -0800

    add non logged in user to organization controller test

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
+  
+  describe "a non-logged in person" do
+    it "redirects to sign in screen" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+      get :show, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+      post :create
+      expect(response).to redirect_to(new_user_session_path)
+      get :edit, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      patch :update, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      put :update, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
 
 end
