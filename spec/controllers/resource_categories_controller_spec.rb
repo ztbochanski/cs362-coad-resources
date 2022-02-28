4269a2cd5f6502357eb6cc6b063f13d02a743d4a
zach <ztbochanski@gmail.com>
2022-02-27 18:50:17 -0800

    add redirect to sign in for index

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe ResourceCategoriesController, type: :controller do
   describe "an unauthenticated person" do
     it "redirects to the sign-in screen" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 
