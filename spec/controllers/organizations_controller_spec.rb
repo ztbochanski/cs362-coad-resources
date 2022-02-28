af6375de11cd0a47ead94a1eb452b1ee3dc020f3
zach <ztbochanski@gmail.com>
2022-02-27 18:15:15 -0800

    remove spacing from org controller not  logged in test block

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -6,22 +6,16 @@ RSpec.describe OrganizationsController, type: :controller do
     it "redirects to sign in screen" do
       get :index
       expect(response).to redirect_to(new_user_session_path)
-
       get :show, params: {id: 'Fake'}
       expect(response).to redirect_to(new_user_session_path)
-
       get :new
       expect(response).to redirect_to(new_user_session_path)
-
       post :create
       expect(response).to redirect_to(new_user_session_path)
-
       get :edit, params: {id: 'Fake'}
       expect(response).to redirect_to(new_user_session_path)
-
       patch :update, params: {id: 'Fake'}
       expect(response).to redirect_to(new_user_session_path)
-
       put :update, params: {id: 'Fake'}
       expect(response).to redirect_to(new_user_session_path)
     end
