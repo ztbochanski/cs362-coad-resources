c676d6b55393bbf5247284936e16cbb1e8938899
zach <ztbochanski@gmail.com>
2022-02-27 16:21:24 -0800

    add index redirects unathenticated person

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
   
-  describe "a person accessing a region" do
+  describe "a unauthenticated person accessing a region" do
 
     it "redirets to the sign in screen" do
       get :index
+      expect(response).to redirect_to(new_user_session_path)
+      get :show, params: {id: "FAKE"}
+      get :new
       expect(response).to redirect_to(new_user_session_path)
     end
   end
