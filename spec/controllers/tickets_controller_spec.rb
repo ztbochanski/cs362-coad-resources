f37cb6bd3314164b9de6d4f2417b5b7d6e85438c
zach <ztbochanski@gmail.com>
2022-02-27 18:33:41 -0800

    add test for capture, release, close, and destroy tickets

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe TicketsController, type: :controller do
       orgranization_user = create(:user)
       orgranization_user.confirm
       sign_in(orgranization_user)
-      delete :destroy, params: {id: 'Fake'}
+      post :capture, params: { id: "Fake" }
+      expect(response).to redirect_to(dashboard_path)
+      post :release, params: { id: "Fake" }
+      expect(response).to redirect_to(dashboard_path)
+      patch :close, params: { id: "Fake" }
+      expect(response).to redirect_to(dashboard_path)
+      delete :destroy, params: {id: "Fake"}
       expect(response).to redirect_to(dashboard_path)
     end
   end
