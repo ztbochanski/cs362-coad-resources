541d78eab39b417507e8ea40a15c96cecff03927
zach <ztbochanski@gmail.com>
2022-02-27 18:31:20 -0800

    remove unathenticated user test that does not make sense

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,17 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
-  describe "an unauthenticated person" do
-    it "redirects to the sign-in screen" do
-    end
-  end
-
   describe "an authenticated user" do
-    it "redirects to the sign-in screen" do
+    it "redirects to the dashboard" do
       orgranization_user = create(:user)
       orgranization_user.confirm
       sign_in(orgranization_user)
+      delete :destroy, params: {id: 'Fake'}
+      expect(response).to redirect_to(dashboard_path)
     end
   end
-  
+
 end
