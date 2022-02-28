6d7ca18328f9831640c799308908c7ab9345dc12
zach <ztbochanski@gmail.com>
2022-02-27 17:36:35 -0800

    add create and edit post request tests

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe RegionsController, type: :controller do
       expect(response).to redirect_to(new_user_session_path)
       get :show, params: {id: "Fake"}
       expect(response).to redirect_to(new_user_session_path)
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+      post :create
+      expect(response).to redirect_to(new_user_session_path)
+      post :edit, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
       
     end
   end
