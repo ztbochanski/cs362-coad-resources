b483c1453c44f5898fffe0d893d1440cf9b40185
zach <ztbochanski@gmail.com>
2022-02-27 18:12:17 -0800

    add get request redirect test for a non logged in user trying to access the dashboard

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
-
+  describe "a non-logged-in person" do
+    it "redirects to the sign-in screen" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
 end
