9caed4b0c5e73ba1f3b2b779fd8a07d381f8ec67
zach <ztbochanski@gmail.com>
2022-02-27 16:31:42 -0800

    add transpo and fake phone to organizations factory

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -2,10 +2,11 @@ FactoryBot.define do
     factory :organization do 
       email { "fake@fake.com" }
       name { "fakename" }
-      phone { "fakephone" }
+      phone { "1-555-666-2244" }
       status { :submitted }
       primary_name { "fake primary name" }
       secondary_name { "fake secondary name" }
-      secondary_phone { "fake secondary phone" }
+      secondary_phone { "1-555-666-2244" }
+      transportation { "yes" }
     end
 end
