af2e3a9ac868ac9ffe62dc958e21e5a686803b91
zach <ztbochanski@gmail.com>
2022-02-27 18:22:29 -0800

    add check for authenticsted user and create a user in the database for testing

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe TicketsController, type: :controller do
     end
   end
 
+  describe "an authenticated user" do
+    it "redirects to the sign-in screen" do
+      orgranization_user = create(:user)
+      orgranization_user.confirm
+      sign_in(orgranization_user)
+    end
+  end
+  
 end
