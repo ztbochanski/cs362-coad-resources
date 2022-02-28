d2f2cebdd7925d66982cf0cd8e610c1e981a8e6a
zach <ztbochanski@gmail.com>
2022-02-27 18:42:31 -0800

    add unath and authed user blocks

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
+  describe "an unauthenticated person" do
+    it "redirects to the sign-in screen" do
+    end
+  end
+
+  describe "an authenticated user" do
+    it "redirects to the dashboard" do
+    end
+  end
 
 end
