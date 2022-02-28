94b0f6fa987d9bec280b456bc0be2a8ac027d37b
zach <ztbochanski@gmail.com>
2022-02-27 18:51:07 -0800

    add redirect to sign in for create

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(response).to redirect_to(new_user_session_path)
       get :new
       expect(response).to redirect_to(new_user_session_path)
+      post :create
+      expect(response).to redirect_to(new_user_session_path)
       
     end
   end
