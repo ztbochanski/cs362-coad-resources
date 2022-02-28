bb60e5c700e5ed5f38ad6d553ff2ec14afec2ada
zach <ztbochanski@gmail.com>
2022-02-27 18:21:14 -0800

    add block for unathenticated person check

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
+  describe "an unauthenticated person" do
+    it "redirects to the sign-in screen" do
+    end
+  end
 
 end
