8ef38d4f8b00a0f524b98f2ca5c5839658372f8c
zach <ztbochanski@gmail.com>
2022-03-02 11:06:01 -0800

    add unauthenticated user test that redirects to sign in
    test a not logged in user trying to access tickets

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
+  
+  describe "an unauthenticated user" do
+    it "redirects to the sign-in screen" do
+      get :show, params: { id: "Fake" }
+      expect(response).to redirect_to(dashboard_path)
+    end
+  end
 
   describe "an authenticated user" do
     it "redirects to the dashboard" do
