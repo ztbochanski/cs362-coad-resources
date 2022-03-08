a6f36b6949e00f7af1e8e286038070e7a7bd4189
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-03-08 11:09:19 -0800

    create ticket system test

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
 
+    describe "creating a ticket" do
+        before do
+            create(:region)
+            create(:resource_category)
+        end
+
+        scenario "non-logged in user creates a ticket successfully" do
+            visit new_ticket_url
+            fill_in "Full Name", with: "FAKE USER"
+            fill_in "Phone Number", with: "8314211816"
+            select 'fake region', from: 'Region'
+            select 'Fake Resource category', from: 'Resource Category'
+            fill_in "Description", with: "Fake Description"
+            click_on "Send this help request"
+            expect(page).to have_text "Ticket Submitted"
+
+            
+        end
+    end
 end
