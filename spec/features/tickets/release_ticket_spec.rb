50ad4d359775d411b624cd20311a92ec77d69f51
zach <ztbochanski@gmail.com>
2022-03-08 12:07:45 -0800

    add test to ensure user who is part of an org can release a ticket
    create the ticket with an approved organization

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Releasing a ticket by an', type: :feature do
+  
+	let(:organization) { create(:organization) }
+
+  describe "a user who is part of an organization" do
+    it "releases a ticket" do
+      organization.approve
+      ticket = create(:ticket, organization: organization)
+    end
+  end
 
 end
