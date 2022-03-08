be857f58fa72d856a033a836fe71c2cfa3cc78e9
Luke Reynolds <lukereynoldspi@gmail.com>
2022-03-08 11:19:09 -0800

    finished application_helper_spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,15 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
+    
     describe "#full_title" do
       it "returns the instance variable" do
-        assign(:page_title, 'Basic Title')
-        expect(helper.full_title).to eq("Basic Title")
+        page_title = "Basic Title"
+        base_title = "Disaster Resource Network"
+        expect(helper.full_title(page_title)).to eq("#{page_title} | #{base_title}")
       end
     end
     describe "#full_title" do
       it "returns the default title" do
-        expect(helper.full_title).to eq("Disaster Resource Network")
+        base_title = "Disaster Resource Network"
+        expect(helper.full_title).to eq(base_title)
       end
     end
 end
