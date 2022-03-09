098cbdc1e860aadf13850f7d4caf225e3c1be36d
zach <ztbochanski@gmail.com>
2022-03-08 17:03:33 -0800

    add approved user captures a ticket and verify the organization name that captured it

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
+  
+  let(:approved_organization) { create(:organization, status: :approved) }
+  let(:user) { create(:user, role: "organization", organization: approved_organization) }
+
+  describe "user who is part of an approved organization" do
+    it "captures a ticket" do
+      ticket = create(:ticket)
+      user.confirm
+      log_in_as(user)
+      expect(page.body).to have_text("Dashboard")
+      click_on "Tickets"
+			click_on ticket.name
+			click_on "Capture"
+      click_on "Tickets"
+			click_on ticket.name
+      expect(page.body).to have_text(approved_organization.name)
+    end
+  end
 
 end
