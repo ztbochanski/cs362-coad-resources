f1c5e9254865972815d288b5398f8d978ba862e5
zach <ztbochanski@gmail.com>
2022-03-08 16:33:17 -0800

    add dom interactions for a logged in user to click on tickets and be able to release the ticket

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe 'Releasing a ticket by an', type: :feature do
+RSpec.describe 'Releasing a ticket by an/a', type: :feature do
   
-	let(:organization) { create(:organization) }
+	let(:approved_organization) { create(:organization, status: :approved) }
+  let(:user) { create(:user, role: "organization", organization: approved_organization) }
 
-  describe "a user who is part of an organization" do
+  describe "user who is part of an organization" do
     it "releases a ticket" do
-      organization.approve
-      ticket = create(:ticket, organization: organization)
+      ticket = create(:ticket, organization: approved_organization)
+      expect(user.role).to eq("organization")
+      user.confirm
+      log_in_as(user)
+      expect(page.body).to have_text("Dashboard")
+      click_on "Tickets"
+			click_on ticket.name
+			click_on "Release"
     end
   end
 
